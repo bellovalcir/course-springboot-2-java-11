8af7bd6246bd76f3cb2e780bf0f128147f60c32e
bellovalcir <bello_soft@hotmail.com>
2021-09-20 07:53:08 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/bello/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.bello.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bello.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
