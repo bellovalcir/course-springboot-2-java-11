65ab28a87eba43b76aff138e7e46cb190c9bd45e
bellovalcir <bello_soft@hotmail.com>
2021-09-22 22:25:16 -0300

    Exception handling - findById

--- src/main/java/com/bello/course/services/UserService.java
+++ src/main/java/com/bello/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.bello.course.entities.User;
 import com.bello.course.repositories.UserRepository;
+import com.bello.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,8 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		//return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -34,11 +36,11 @@ public class UserService {
 	
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
-		updateData(entity, obj);
+		updateData(entity, obj); //????
 		return repository.save(entity);
 	}
 
-	private void updateData(User entity, User obj) {
+	private void updateData(User entity, User obj) { //????
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
